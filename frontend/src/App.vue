536e5e6c6b3a1e04a0e854e2f0e0b252bd288793
Naimul islam <mdnaimul2090@gmail.com>
2024-11-10 00:08:46 +0600

    setup project

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import axios from 'axios';
+// setup script
+const formSubmit = async () => {
+    await axios.get("/sanctum/csrf-cookie");
+    await axios.post("/login");
+    console.log('Form Submitted');
+}
+</script>
+
+<template>
+    <div>
+        <h1>Vue 3 App</h1>
+        <form @submit.prevent="formSubmit">
+            <input type="text" placeholder="Enter your name" /> <br>
+            <input type="text" placeholder="Enter your name" /> <br>
+            <button type="submit">login</button>
+        </form>
+    </div>
+</template>
+
+<style scoped>
+/* scoped style  */
+</style>
